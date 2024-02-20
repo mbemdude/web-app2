7f6ea296631093195e1632f7edc3ba27f57be27d
mbemdude <ala52798@gmail.com>
2024-02-20 10:26:46 +0800

    Login & Dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -10,9 +12,13 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
-|
+
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Login Session
+Route::get('/login',[LoginController::class,'index'])->name('login');
+Route::post('/login-proses',[LoginController::class, 'login_proses'])->name('login-proses');
+Route::post('/logout',[LoginController::class, 'logout'])->name('logout');
+
+// Dashboard
+Route::get('/dashboard', [HomeController::class,'dashboard'])->name('dashboard');
